5f2ccaacb6492eb051443d6d9e2ba6d7c614809e
charanNagaraj <charansharan2004@gmail.com>
2024-11-14 22:41:07 +0530

    check toggle

--- routes/pi.js
+++ routes/pi.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router();
-
+const toggle = require('../hardware/lights')
 router.route('/').get((req,res)=>{
     res.status(200).render('devices/pi')
 })
 router.route('/camera').get((req,res)=>res.render('camera'))
-router.route('/lights').get((req,res)=>res.render('devices/pi/lights'))
+router.route('/lights')
+.get((req,res)=>res.render('devices/pi/lights'))
+.post((req,res)=>{
+    toggle()
+    res.redirect('/pi/lights')
+})
 
 module.exports = router
