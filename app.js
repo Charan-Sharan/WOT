8046dbf23ce8f813d846889caab0db651c9ed4f0
charanNagaraj <charansharan2004@gmail.com>
2024-11-19 17:57:16 +0530

    refactor

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app =  express()
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require('socket.io');
-// const {Server} = socketIo(server);
 const io = new Server(server)
 const path = require('path')
 const dotenv = require('dotenv').config();
@@ -13,31 +12,16 @@ const ejsMate = require('ejs-mate')
 const { Gpio } = require('onoff');
 const { exec } = require('child_process');
 
-
 const PORT = process.env.PORT || 5001
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(express.static('public')); 
-
-
 app.set('view engine','ejs')
 app.engine('ejs',ejsMate)
 app.set('views',path.join(__dirname,'views'))
-const { PiGpio } = require('pigpio');
-// const lightPin = new PiGpio(529, 'out'); //17
-const redPin = new PiGpio(18, { mode: Gpio.OUTPUT });   //18
-const greenPin = new PiGpio(4, { mode: Gpio.OUTPUT });   //04
-const bluePin = new PiGpio(23, { mode: Gpio.OUTPUT });    //23
 
-
-
-// Set up a PWM pin
-
-
-// Write an analog value (duty cycle between 0 and 255)
- // 50% brightness (0 = off, 255 = fully on)
 
 io.on('connection', (socket) => {
     console.log('A user connected');
@@ -47,14 +31,13 @@ io.on('connection', (socket) => {
         const r = ( parsedColor & 0xff0000 ) >> 16
         const g = ( parsedColor & 0x00ff00 ) >>8
         const b = parsedColor & 0x0000ff
-        greenPin.pwmWrite(g);
-        redPin.pwmWrite(r);
-        bluePin.pwmWrite(b);
         console.log(`${color}=> r : ${r} || g : ${g} || b : ${b}`)
     });
 });
-app.use('/pi',pi)
+
+
 app.get('/',(re,res)=>res.render('home',{title:"home"}))
+app.use('/pi',pi)
 app.get('/camera',(req,res)=>{
     res.render('sensors/camera')
 })
